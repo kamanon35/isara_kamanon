2408211637a32ec95dc067de7553f5983d9648d1
unknown <k.amano.javajava@gmail.com>
2020-01-17 21:23:39 +0900

    よくある質問アコーディオン実装

--- js/home.js
+++ js/home.js
@@ -3,19 +3,25 @@ $(function(){
   // アコーディオン１つ目を開く
   $('.open').show();
 
-  var headerHeight = $('header').height();
+  $('.contactform-jump').click(function(){
 
-  $('.contact-form-jump a, .header-right a').click(function(){
-
+    var headerHeight = $('header').height();
     var id = $(this).attr('href');
     var position = $(id).offset().top - headerHeight;
-    var scrollSpeed = 300;
+    var scrollSpeed = 500;
 
     $('html, body').animate({
-      'scrollTop': position
+      scrollTop: position
     }, scrollSpeed);
 
     return false;
+  });
+
+  $('.question-container').click(function(){
+
+    var $toggleTarget = $(this).next('.question-description');
+    var $toggleAngle = $(this).find('.fa-angle-down, .fa-angle-up');
+    toggleChangePanel($toggleTarget, $toggleAngle);
   });
 
   // small screenのみ動作
@@ -25,23 +31,34 @@ $(function(){
       var $toggleTarget = $(this).parents('.attending-flow-box').find('.attending-flow-box-bottom');
       var $toggleAngle = $(this).parents('.attending-flow-box').find('.fa-angle-down, .fa-angle-up');
 
-      if($toggleTarget.hasClass('open')){
-        // スライド格納
-        $toggleTarget.removeClass('open');
-        $toggleTarget.slideUp();
-
-        // 矢印ボタンの切り替え
-        $toggleAngle.removeClass('fa-angle-up').addClass('fa-angle-down');
-
-      } else {
-        // スライド表示
-        $toggleTarget.addClass('open');
-        $toggleTarget.slideDown();
-
-        // 矢印ボタンの切り替え
-        $toggleAngle.removeClass('fa-angle-down').addClass('fa-angle-up');
-
-      }
+      toggleChangePanel($toggleTarget, $toggleAngle);
     });
+  }
+
+  // 一番上に戻るボタン
+  $('#pagetop').click(function(){
+    $('body, html').animate({ scrollTop: 0 }, 500); //0.5秒かけてトップへ戻る
+    return false;
+  });
+
+  // panelの表示・非表示を切り替え
+  function toggleChangePanel($toggleTarget, $toggleAngle){
+
+    if($toggleTarget.hasClass('open')){
+      // スライド格納
+      $toggleTarget.removeClass('open');
+      $toggleTarget.slideUp(300, 'swing');
+
+      // 矢印ボタンの切り替え
+      $toggleAngle.removeClass('fa-angle-up').addClass('fa-angle-down');
+
+    } else {
+      // スライド表示
+      $toggleTarget.addClass('open');
+      $toggleTarget.slideDown(300, 'swing');
+
+      // 矢印ボタンの切り替え
+      $toggleAngle.removeClass('fa-angle-down').addClass('fa-angle-up');
+    }
   }
 });
